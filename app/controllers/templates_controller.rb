731af5c5184ec9c6afa746353fca793b1ef7de6a
luizeduardoreis <luizedureis1504@gmail.com>
2024-06-20 14:51:35 -0300

    Removed Templates

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,50 +1,51 @@
 class TemplatesController < ApplicationController
-    before_action :set_controller, except: [:index, :new, :create]
+    before_action :set_template, except: [:index, :new, :create]
+  
     def index 
-        @templates = Template.all
+      @templates = Template.all
     end
-
+  
     def show
     end
-
+  
     def new
-        @template = Template.new
+      @template = Template.new
     end
-
+  
     def create 
-        @template = Template.new(template_params)
-        if @template.save
-            redirect_to @template
-        else
-            render :new, status: :unprocessable_entity
-        end
+      @template = Template.new(template_params)
+      if @template.save
+        redirect_to templates_path
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
-
+  
     def edit
     end
-
+  
     def update 
-        if @template.update(template_params)
-            redirect_to @template
-        else
-            render :edit, status: :unprocessable_entity
-        end
+      if @template.update(template_params)
+        redirect_to @template
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
-
+  
     def destroy 
-        @template.destroy
-        redirect_to root_path   
+      @template.destroy
+      redirect_to root_path   
     end
-
+  
     private
-
+  
     def template_params
-        params.require(:template).permit(:name, :content)
+      params.require(:template).permit(:name, :content)
     end
-
+  
     def set_template
-        @template = Template.find(params[:id])
+      @template = Template.find(params[:id])
     rescue ActiveRecord::RecordNotFound
-        redirect_to root_path
+      redirect_to root_path
     end
-end
+  end
--- spec/factories/templates.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :template do
-    name { "MyString" }
-    content { "MyString" }
-  end
-end
