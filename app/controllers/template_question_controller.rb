36b9b0c48c470056d6983db2d934488a8f8ec496
Mateus Santos <santos.mateus@protonmail.com>
2024-07-04 10:36:24 -0300

    implemented alternatives controller

--- app/controllers/template_question_controller.rb
+++ app/controllers/template_question_controller.rb
@@ -8,4 +8,21 @@ class TemplateQuestionController < ApplicationController
 
     redirect_to edit_template_path(template_id)
   end
+
+  def update
+    id = params[:id].to_i
+    @template_question = TemplateQuestion.find(id)
+
+    template_question_params = params[:template_question]
+    if template_question_params[:question_type] == "text"
+      @template_question.update(question_type: "text", title: template_question_params[:question], content: "")
+    elsif template_question_params[:question_type] == "radio"
+      @template_question.update(question_type: "radio", title: template_question_params[:question], content: template_question_params[:alternatives].to_json)
+    elsif template_question_params[:question_type] == "checkbox"
+      @template_question.update(question_type: "checkbox", title: template_question_params[:question], content: template_question_params[:alternatives].to_json)
+    else
+      flash[:alert] = "Question type #{template_question_params[:question_type]} does not exist!"
+      redirect_to edit_template_path(params[:template_id])
+    end
+  end
 end
--- spec/controller/template_question_spec.rb
+++ spec/controller/template_question_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe TemplateQuestionController, type: :controller do
+  let(:user) { FactoryBot.create(:user, admin: true) }
+
+  before :each do
+    sign_in user
+  end
+
   describe "add question" do
-    let(:user) { FactoryBot.create(:user, admin: true) }
-
-    before :each do
-      sign_in user
-    end
-
-    it "find the current template" do
+    it "should find the current template" do
       template = double("template")
       expect(Template).to receive(:find).with(17).and_return(template)
       allow(template).to receive(:template_questions).and_return([])
@@ -28,6 +28,38 @@ RSpec.describe TemplateQuestionController, type: :controller do
 
       post :create, params: { template_id: "17" }
     end
+  end
 
+  describe "edit question" do
+    it "should find the current template question" do
+      template_question = double("template_question")
+      expect(TemplateQuestion).to receive(:find).with(13).and_return(template_question)
+
+      post :update, params: { template_id: "17", id: "13" }
+    end
+
+    it "should update the question - text type" do
+      template_question = double("template_question")
+      allow(TemplateQuestion).to receive(:find).with(13).and_return(template_question)
+      expect(template_question).to receive(:update).with({ title: "Q1", question_type: "text", content: "" })
+
+      post :update, params: { template_id: "17", id: "13", template_question: { question: "Q1", question_type: "text" } }
+    end
+
+    it "should update the question - radio type" do
+      template_question = double("template_question")
+      allow(TemplateQuestion).to receive(:find).with(13).and_return(template_question)
+      expect(template_question).to receive(:update).with({ title: "Q1", question_type: "radio", content: "[\"1\",\"2\",\"3\"]" })
+
+      post :update, params: { template_id: "17", id: "13", template_question: { question: "Q1", question_type: "radio", alternatives: %w[1 2 3] } }
+    end
+
+    it "should update the question - checkbox type" do
+      template_question = double("template_question")
+      allow(TemplateQuestion).to receive(:find).with(13).and_return(template_question)
+      expect(template_question).to receive(:update).with({ title: "Q1", question_type: "checkbox", content: "[\"1\",\"2\",\"3\"]" })
+
+      post :update, params: { template_id: "17", id: "13", template_question: { question: "Q1", question_type: "checkbox", alternatives: %w[1 2 3] } }
+    end
   end
 end
