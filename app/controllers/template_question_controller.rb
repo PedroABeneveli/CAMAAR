d5f0a94874543426aa1cd3bc7f30e2fc159b2753
Mateus Santos <santos.mateus@protonmail.com>
2024-07-04 09:42:48 -0300

    implemented add question button on view

--- app/controllers/template_question_controller.rb
+++ app/controllers/template_question_controller.rb
@@ -1,7 +1,11 @@
 class TemplateQuestionController < ApplicationController
   def create
-    @template = Template.find(params[:template_id].to_i)
+    template_id = params[:template_id].to_i
+
+    @template = Template.find(template_id)
     @template.template_questions << TemplateQuestion.create(question_type: "text", title: "", content: "")
     @template.save!
+
+    redirect_to edit_template_path(template_id)
   end
 end
