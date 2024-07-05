3e3a8092d3dbce7b67804e60d9c972cda1a154db
Mateus Santos <santos.mateus@protonmail.com>
2024-07-05 15:39:19 -0300

    started refactor of send_forms_controller

--- app/controllers/send_forms_controller.rb
+++ app/controllers/send_forms_controller.rb
@@ -7,22 +7,33 @@ class SendFormsController < ApplicationController
 
   def create
     template = Template.find(params[:template].to_i)
-    
-    params.each do |k, v|
-      if k.starts_with?("study_class_") && v == "1"
-        sc_id = k[12..].to_i
 
-        sc = StudyClass.find(sc_id)
-        sc.users.each do |u|
-          if FormRequest.find_by(user_id: u.id, study_class_id: sc.id).nil?
-            FormRequest.create(template: template, study_class: sc, user: u)
-          end
-        end
+    params.each do |key, value|
+      if is_study_class_selected(key, value)
+        study_class = param_value_to_study_class(key)
+        study_class.send_form_to_users(template)
 
-        if sc.docente.present? && FormRequest.find_by(user_id: sc.docente.id, study_class_id: sc.id).nil?
-          FormRequest.create(template: template, study_class: sc, user: sc.docente)
-        end
+        # study_class.users.each do |user|
+        #   FormRequest.send(user, study_class)
+        #   if FormRequest.find_by(user_id: u.id, study_class_id: sc.id).nil?
+        #     FormRequest.create(template: template, study_class: sc, user: u)
+        #   end
+        # end
+
+        # if sc.docente.present? && FormRequest.find_by(user_id: sc.docente.id, study_class_id: sc.id).nil?
+        #   FormRequest.create(template: template, study_class: sc, user: sc.docente)
+        # end
       end
     end
+  end
+
+  private
+
+  def param_value_to_study_class(key)
+    StudyClass.find(key[12..].to_i)
+  end
+
+  def is_study_class_selected(key, value)
+    key.starts_with?("study_class_") && value == "1"
   end
 end
--- spec/controller/send_forms_controller_spec.rb
+++ spec/controller/send_forms_controller_spec.rb
@@ -21,5 +21,37 @@ RSpec.describe SendFormsController, type: :controller do
 
       post :create, params: { template: "17" }
     end
+
+    it "should find the selected classes" do
+      template = double("template")
+      allow(Template).to receive(:find).with(17).and_return(template)
+
+      sc1 = double("sc1")
+      sc3 = double("sc3")
+
+      expect(StudyClass).to receive(:find).with(1).and_return(sc1)
+      expect(StudyClass).to receive(:find).with(3).and_return(sc3)
+
+      allow(sc1).to receive(:send_form_to_users)
+      allow(sc3).to receive(:send_form_to_users)
+
+      post :create, params: { template: "17", study_class_1: "1", study_class_3: "1" }
+    end
+
+    it "should send form to users for the selected classes" do
+      template = double("template")
+      allow(Template).to receive(:find).with(17).and_return(template)
+
+      sc1 = double("sc1")
+      sc3 = double("sc3")
+
+      allow(StudyClass).to receive(:find).with(1).and_return(sc1)
+      allow(StudyClass).to receive(:find).with(3).and_return(sc3)
+
+      expect(sc1).to receive(:send_form_to_users)
+      expect(sc3).to receive(:send_form_to_users)
+
+      post :create, params: { template: "17", study_class_1: "1", study_class_3: "1" }
+    end
   end
 end
