bb04b766ec9c51171b1f119e270e629dca1d6325
Mateus Santos <santos.mateus@protonmail.com>
2024-07-05 17:22:32 -0300

    removed boilerplate tests

--- spec/requests/avaliacoes_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Avaliacoes", type: :request do
-  describe "GET /index" do
-    it "should have a index view" do
-      pending
-    end
-  end
-end
--- spec/requests/response_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Responses", type: :request do
-  describe "GET /show" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
