4779b577d62611d77f96c7010ca6376f5d23b4e8
Gubsb <nogueiragustavo207@gmail.com>
2024-07-05 20:44:58 -0300

    documrntation for some controllers

--- app/controllers/avaliacoes_controller.rb
+++ app/controllers/avaliacoes_controller.rb
@@ -1,14 +1,12 @@
-##
-# O controlador AvaliacoesController gerencia as avaliações dos usuários.
-# Exige autenticação do usuário para todas as ações.
+# Controlador responsável pelo tratamento das requisições referentes 
+# à view de visualização de formulários
 class AvaliacoesController < ApplicationController
   before_action :authenticate_user!
 
-  ##
   # Lista os formulários pendentes do usuário atual.
   #
-  # @return [void] Não retorna um valor diretamente.
-  # @effect Define a variável de instância @forms e renderiza a página com o layout "home".
+  # Não retorna valor.
+  # Tem como efeito colateral a renderização da página com os formulários disponíveis
   def index
     @forms = current_user.find_pending_forms
     render layout: "home"
--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -1,9 +1,12 @@
+# Controlador responsável pelo tratamento das requisições referentes 
+# à view de preenchimento e envio de um formulário
 class ResponseController < AvaliacoesController
-  # Método para renderizar a página de índice de respostas
+
+  # Método para renderizar a página do template
   #
   # Não recebe argumentos.
   # Não retorna valor.
-  # Não possui efeitos colaterais além de renderizar a página com layout "home".
+  # Tem como efeito colateral a renderização da página do template
   def index
     @form_request = FormRequest.find(params[:avaliaco_id])
     render layout: "home"
@@ -15,8 +18,10 @@ class ResponseController < AvaliacoesController
   # - params: Um hash contendo os parâmetros da requisição HTTP.
   #
   # Não retorna valor explícito.
-  # Pode ter efeitos colaterais: criação de um novo registro de FormResponse no banco de dados,
-  # marcação do FormRequest como respondido (answered = true) e redirecionamento para outra página.
+  # Pode ter efeitos colaterais: 
+  # - criação de um novo registro de FormResponse no banco de dados
+  # - marcação do FormRequest como respondido (answered = true) 
+  # - redirecionamento a página de avaliações
   def create
     @form_request = FormRequest.find(params[:avaliaco_id])
 
