44fd61c15f220f306a286ca9d304b3dba855f826
Pedro Beneveli <pedro.beneveli@gmail.com>
2024-07-05 21:27:58 -0300

    RDoc FormRequest

--- app/models/form_request.rb
+++ app/models/form_request.rb
@@ -8,6 +8,15 @@ class FormRequest < ApplicationRecord
   belongs_to :user
   belongs_to :template
 
+  # Cria um novo form request, que liga um template com um usuário e uma turma, para que o aluno possa responder
+  #
+  # Recebe como parâmetros:
+  # - template: um Template referente ao formato do formulário a ser enviado
+  # - user: um User que receberá o formulário a ser respondido
+  # - study_class: um StudyClass referente a qual turma deverá ser avaliada naquele formulário
+  #
+  # Não há retorno.
+  # Esse método possui o efeito colateral de possivelmente adicionar uma instância de FormRequest caso não exista uma relação entre os Template, User e StudyClass fornecidos como parâmetro.
   def self.send_form(template, user, study_class)
     fr = FormRequest.find_by({ template_id: template.id, user_id: user.id, study_class_id: study_class.id })
 
